bc12c45a80d40e2207f04ceb430c8b88b46c491f
kylen_morris <kmorris@omnilogic.net>
2023-10-21 17:46:09 -0600

    * minor refactor, can view announcement(s) - need to add create/edit and separate into modules

---
+++ ManagementSuite/src/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+from config import Config
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize Flask extensions here
+
+    # Register blueprints here
+    from main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    @app.route('/test/')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+    return app
