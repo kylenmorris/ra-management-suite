2c020ea0c882ba5bbabe4c7eb85be84fdf001be5
Leo-YiLuan <leo.yiluan@gmail.com>
2023-12-01 20:31:52 -0600

    fix announcement banner popped up on task page

--- RAManagementSuite/app.py
+++ RAManagementSuite/app.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     app.debug = True
-    # app.run(host='0.0.0.0', port=8080)
+    # app.run(host='0.0.0.0', port=8080)   # Use this entry when use docker, open 127.0.0.1:8080 on your browser
     app.run()
